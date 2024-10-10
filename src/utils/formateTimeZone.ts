5b1e7506aa775e0d6e930eb84351bbbe591e740a
DW-Roshan <roshan@dreamweaversindia.com>
2024-10-10 12:03:48 +0530

    Fixed TimeZone

--- src/utils/formateTimeZone.ts
+++ src/utils/formateTimeZone.ts
@@ -2,5 +2,5 @@ import { formatInTimeZone } from 'date-fns-tz';
 
 
 export function formateTimeZone(date: Date) {
-  return formatInTimeZone(date, `${process.env.TZ}`, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+  return formatInTimeZone(date, `${process.env.MY_APP_TIMEZONE}`, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 }
