22e3c46347cd0a4923c8421afec3218dea5987a5
DW-Roshan <roshan@dreamweaversindia.com>
2025-11-21 15:54:19 +0530

    changes in feedback page

--- src/app/[lang]/(student-examination)/feedback/page.tsx
+++ src/app/[lang]/(student-examination)/feedback/page.tsx
@@ -1,8 +1,52 @@
-"use client"
+// "use client"
+
+// import { Grid } from "@mui/material";
+
+// const FeedBackPage = () => {
+
+//   return (
+//     <div className='p-4'>
+//       <Grid container spacing={6}>
+//         <Grid item xs={12}>
+//           Feedback Page
+//         </Grid>
+//         <Grid item sm={12} md={7}>
+//           Thank you!!
+//         </Grid>
+//       </Grid>
+//     </div>
+//   );
+
+// }
+
+// export default FeedBackPage
+
+
+"use client";
+
+import { useEffect } from "react";
 
 import { Grid } from "@mui/material";
 
 const FeedBackPage = () => {
+
+  useEffect(() => {
+    // Push a new state so back button doesn't immediately go to previous page
+    history.pushState(null, "", location.href);
+
+    const handleBack = () => {
+      // When user presses back → close window
+      window.close();
+    };
+
+    // Listen to back/forward
+    window.addEventListener("popstate", handleBack);
+
+    return () => {
+      window.removeEventListener("popstate", handleBack);
+    };
+  }, []);
+
 
   return (
     <div className='p-4'>
@@ -19,4 +63,4 @@ const FeedBackPage = () => {
 
 }
 
-export default FeedBackPage
+export default FeedBackPage;
