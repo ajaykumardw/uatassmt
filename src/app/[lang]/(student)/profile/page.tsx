f2a457678665d85f97fc8b735f7f58b5039c9ed2
DW-Roshan <roshan@dreamweaversindia.com>
2026-04-08 15:54:46 +0530

    fixed build issue

---
+++ src/app/[lang]/(student)/profile/page.tsx
@@ -0,0 +1,11 @@
+
+const StudentProfile = async () => {
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Student Profile</h1>
+    </div>
+  )
+}
+
+export default StudentProfile
